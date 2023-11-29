ef7a2910dae8639b465e9a839791fbec83e4305b
4701104091 <4701104091@student.hcmue.edu.vn>
2023-11-29 14:51:05 +0700

    Extend Identity User

--- Telescope.DataAccess/Migrations/20231129074507_ExtendIdentityUser.Designer.cs
+++ Telescope.DataAccess/Migrations/20231129074507_ExtendIdentityUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Telescope.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using Telescope.DataAccess.Data;
 namespace Telescope.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231129074507_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
