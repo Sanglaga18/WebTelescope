5805402c589ccf643de4d37419d8b8114a6cd0f4
Tran Minh Khoi <khoitk14033@gmail.com>
2023-11-29 09:45:31 +0700

    Home Page

--- Telescope/Areas/Customer/Controllers/HomeController.cs
+++ Telescope/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Telescope.Models;
+
 
 namespace Telescope.Areas.Customer.Controllers
 {
@@ -8,15 +10,17 @@ namespace Telescope.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
