899b334c042f3b47008a01821948d740456bba39
4701104091 <4701104091@student.hcmue.edu.vn>
2023-11-30 14:55:11 +0700

    Add Shopping Cart to Repository

---
+++ Telescope.DataAccess/Repository/ShoppingCartRepositor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telescope.DataAccess.Data;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models;
+
+namespace Telescope.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
+}
