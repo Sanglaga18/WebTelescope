4c15bf651df990c3356b5bf447b99b744ddef9d5
Tran Minh Khoi <khoitk14033@gmail.com>
2023-11-30 20:22:56 +0700

    Get Shopping Cart

--- Telescope/Areas/Customer/Controllers/CartController.cs
+++ Telescope/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models.ViewModels;
 
 namespace Telescope.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
