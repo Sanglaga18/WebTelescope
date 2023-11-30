890aeb12620ad3513a95f77017e4f5b01b2fbf5b
Tran Minh Khoi <khoitk14033@gmail.com>
2023-12-01 00:11:19 +0700

    Add Order Header and Detail Repository

---
+++ Telescope.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telescope.DataAccess.Data;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models;
+
+namespace Telescope.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
---
+++ Telescope.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telescope.DataAccess.Data;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models;
+
+namespace Telescope.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
