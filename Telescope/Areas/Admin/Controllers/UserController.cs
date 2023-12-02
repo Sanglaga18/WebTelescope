446ef3c5228f262fd12181bba34766f27673e261
4701104091 <4701104091@student.hcmue.edu.vn>
2023-12-03 04:29:32 +0700

    Display User List

---
+++ Telescope/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using Telescope.DataAccess.Data;
+using Telescope.DataAccess.Repository.IRepository;
+using Telescope.Models;
+using Telescope.Models.ViewModels;
+using Telescope.Utility;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Telescope.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
